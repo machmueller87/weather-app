f66e1e36f0356b8a022e53eeb93a02a383e5b28d
machmueller87 <89366673+machmueller87@users.noreply.github.com>
2021-08-27 11:34:04 -0500

    Updated CSS and JS

---
+++ src/app.js
@@ -0,0 +1,53 @@
+// Current Date
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#currentDate");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+//
+//
+//
+//
+//
+//
+// Search + API
+function displayWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("h3").innerHTML = Math.round(response.data.main.temp);
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "ea97b57ce7c5e5f67f7b9f59b21d70bf";
+  let city = document.querySelector("#search-text-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let searchInput = document.querySelector("#search-text-input");
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
